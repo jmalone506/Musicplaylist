daa7ed7aa321537d02ee20a476f86fd5cb975ff8
Joshua Malone <jcharlesmalone@gmail.com>
2021-03-02 10:21:03 -0800

    "added seeds" ~

---
+++ database/seeds.sql
@@ -0,0 +1,24 @@
+
+USE musicplaylist_db;
+
+INSERT INTO user (firstname, lastname) 
+VALUES ("Josh", "Malone");
+INSERT INTO user (firstname, lastname) 
+VALUES ("Adam", "Juwig");
+INSERT INTO user (firstname, lastname) 
+VALUES ("Jammie", "Hardy");
+
+INSERT INTO playlist(genre, song, artist)
+VALUES("Rock", "Crazy Train", "Ozzy Osbourne");
+INSERT INTO playlist(genre, song, artist)
+VALUES("Rock", "We Will Rock You", "Queen");
+INSERT INTO playlist(genre, song, artist)
+VALUES("Rock", "back in black", "AC/DC");
+INSERT INTO playlist(genre, song, artist)
+VALUES("hip-hop", "mama said knock you out", "LL Cool J");
+INSERT INTO playlist(genre, song, artist)
+VALUES("Rock", "The Game", "Motorhead");
+INSERT INTO playlist(genre, song, artist)
+VALUES("Rock", "Radioactive", "Imagine Dragon");
+INSERT INTO playlist(genre, song, artist)
+VALUES("hip-hop", "Till I Collapse", "Eminem");
